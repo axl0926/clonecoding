489e02ef6c22fee49cf40050c319399245bc7543
axl0926 <song22092000@gmail.com>
2021-11-24 16:46:53 +0900

    링크 제외 베라  마무리

--- baskinrobbins/js/slideBanner.js
+++ baskinrobbins/js/slideBanner.js
@@ -46,22 +46,17 @@ function moveSlide(slideNum = 1, isNext = true) {
 
     nextSlide.style.transition = 'transform 0s';
     nextSlide.style.transform = `translate(${slideDir*100}vw)`;
-    console.log(`다음 슬라이드 위치 ${slideDir*100}vw`);
     setTimeout(() => {
         nextSlide.style.transition = 'transform 0.4s';
         nextSlide.style.transform = 'translate(0px)';
 
         activeSlide.style.transition = 'transform 0.4s';
         activeSlide.style.transform = `translate(${slideDir*-100}vw)`;
-        console.log(`현재 슬라이드 위치${slideDir*-100}으로 이동`);
         setTimeout(() => {
             activeSlide.classList.remove('sl-active');
             nextSlide.classList.add('sl-active');
-            console.log(`현재 슬라이드 클래스 리스트${activeSlide.classList}`);
-            console.log(`다음 슬라이드 클래스 리스트${nextSlide.classList}`);
             slideNavs[slideIndex].classList.remove('sl-nav-active');
             slideNavs[nextSlideIndex].classList.add('sl-nav-active');
-            console.log(`${tmp}회 반복종료--------------------------------------------`);
         }, 0);
     }, 0);
     lastSlideIndex = nextSlideIndex;
