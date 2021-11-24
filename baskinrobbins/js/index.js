c009da1868ccedcc6e59e693df6f8bc45f550b0d
axl0926 <song22092000@gmail.com>
2021-11-24 15:29:42 +0900

    이벤트리스트 중간커밋

--- baskinrobbins/js/index.js
+++ baskinrobbins/js/index.js
@@ -1,9 +1,16 @@
+var body = document.body,
+    html = document.documentElement;
 
+var domHeight = Math.max( body.scrollHeight, body.offsetHeight, 
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+document.querySelector('.search-shadow-bg').style.height=domHeight+'px';
 
 let searchToggle=true;
 const searchBtn=document.querySelector('#search');
 const searchGrp=document.querySelector('.search-group');
 const searchBg=document.querySelector('.search-shadow-bg');
+const nav=document.querySelector('nav');
 
 searchBtn.addEventListener('click',()=>{
     console.log(searchBtn);
@@ -14,12 +21,26 @@ searchBtn.addEventListener('click',()=>{
         // searchGrp.style.opacity='1';
         searchGrp.classList.add('search-active');
         searchBg.classList.add('search-active');
+        nav.classList.add('no-hover');
+
         searchToggle=!searchToggle;
     }
     else{
         searchBtn.style.background='url(./img/icon_search.png) no-repeat';
         searchGrp.classList.remove('search-active');
         searchBg.classList.remove('search-active');
+        nav.classList.remove('no-hover');
         searchToggle=!searchToggle;
     }
+});
+
+
+const searchHashtagBtn=document.querySelectorAll('.search-hashtag-fav button');
+const inputHashtag=document.querySelector('#hashtag');
+
+searchHashtagBtn.forEach((e,i)=>{
+    e.addEventListener('click',ev=>{
+        ev.preventDefault();
+        inputHashtag.value=searchHashtagBtn[i].innerHTML;
+    });
 });
--- baskinrobbins/js/slideBanner.js
+++ baskinrobbins/js/slideBanner.js
@@ -1,29 +1,28 @@
-const slideNavs=document.querySelectorAll('#banner_slide_toggle>li');
-var lastSlideIndex=0;
+
+
+
+
+const btns = document.querySelectorAll('#main_banner button');
+const slideNavs = document.querySelectorAll('#banner_slide_toggle>li');
+var lastSlideIndex = 0;
 let timerAutoSlide;
-window.addEventListener('focus', ()=>{
-    timerAutoSlide = setInterval(() => moveSlide(1, true), 5000);
+let tmp = 0;
+window.addEventListener('focus', () => {
+    timerAutoSlide = setInterval(() => moveSlide(1, true), 4000);
 });
 
-window.addEventListener('blur', ()=>{
+window.addEventListener('blur', () => {
     clearInterval(timerAutoSlide);
 });
 
-slideNavs.forEach((e,i)=> {
-    e.addEventListener('click',()=>{
-        
-        if(i>lastSlideIndex){
 
-            console.log('slideIndex==='+lastSlideIndex);
-            console.log('i==='+i);
-            console.log(`${(5+i-lastSlideIndex)%5}칸 오른쪽으로 이동`);
-            moveSlide(i-lastSlideIndex,true);
-        }
-        else if(i<lastSlideIndex){
-            console.log('slideIndex==='+lastSlideIndex);
-            console.log('i==='+i);
-            console.log(`${(5-i+lastSlideIndex)%5}칸 왼쪽으로 이동`);
-            moveSlide((5-i+lastSlideIndex)%5,false);
+slideNavs.forEach((e, i) => {
+    e.addEventListener('click', () => {
+
+        if (i > lastSlideIndex) {
+            moveSlide(i - lastSlideIndex, true);
+        } else if (i < lastSlideIndex) {
+            moveSlide((5 - i + lastSlideIndex) % 5, false);
         }
     });
     e.addEventListener('mouseenter', () => {
@@ -31,7 +30,7 @@ slideNavs.forEach((e,i)=> {
     });
     e.addEventListener('mouseleave', () => {
 
-        timerAutoSlide = setInterval(() => moveSlide(1, true), 5000);
+        timerAutoSlide = setInterval(() => moveSlide(1, true), 4000);
     });
 });
 
@@ -43,20 +42,32 @@ function moveSlide(slideNum = 1, isNext = true) {
     const slideIndex = Number(activeSlide.classList[0].substr(8));
     const nextSlideIndex = (5 + slideIndex + slideDir * slideNum) % 5;
     const nextSlide = document.querySelector(`.sl-inner${nextSlideIndex}`);
+    tmp++;
+    console.log(`${tmp}회 반복시작------------------------------------`);
 
     nextSlide.style.transition = 'transform 0s';
     nextSlide.style.transform = `translate(${slideDir*100}vw)`;
+    console.log(`다음 슬라이드 위치 ${slideDir*100}vw`);
     setTimeout(() => {
         nextSlide.style.transition = 'transform 0.4s';
         nextSlide.style.transform = 'translate(0px)';
+
+        console.log(`다음 슬라이드 위치 뷰포트로 이동`);
         activeSlide.style.transition = 'transform 0.4s';
         activeSlide.style.transform = `translate(${slideDir*-100}vw)`;
+        console.log(`현재 슬라이드 위치${slideDir*-100}으로 이동`);
         setTimeout(() => {
             activeSlide.classList.remove('sl-active');
             nextSlide.classList.add('sl-active');
+            console.log(`현재 슬라이드 클래스 리스트${activeSlide.classList}`);
+            console.log(`다음 슬라이드 클래스 리스트${nextSlide.classList}`);
             slideNavs[slideIndex].classList.remove('sl-nav-active');
             slideNavs[nextSlideIndex].classList.add('sl-nav-active');
+            console.log(`${tmp}회 반복종료--------------------------------------------`);
         }, 0);
     }, 0);
-    lastSlideIndex=nextSlideIndex;
+    lastSlideIndex = nextSlideIndex;
+
 }
+
+
